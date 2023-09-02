181fe0c39b34f2b2b4fc16661a0d3bf38478e8c1
alliedeaver <adeav97@gmail.com>
2023-09-02 11:23:01 -0700

    added js and css elements

---
+++ script.js
@@ -0,0 +1,29 @@
+let APIkey = "6d6329217dd25b2852f4e4c6ed79641c";
+let Atlanta
+let Denver
+let Seattle
+let SanFrancisco
+let Orlando
+let NewYork
+let Chicago
+let Austin
+//make the call using the fetch(queryURL) function 
+
+
+api.openweathermap.org/data/2.5/weather?q={city}&appid= {APIkey}
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + Atlanta + "&appid=" + APIkey;
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + Denver + "&appid=" + APIkey;
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + Seattle + "&appid=" + APIkey;
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + SanFrancisco + "&appid=" + APIkey;
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + Orlando + "&appid=" + APIkey;
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + NewYork + "&appid=" + APIkey;
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + Chicago + "&appid=" + APIkey;
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + Austin + "&appid=" + APIkey;
